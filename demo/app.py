f79e7a64dee26abb472b1643e21db66f3178c77b
Cezar Sa Espinola <cezarsa@gmail.com>
2016-11-18 17:46:18 -0200

    ajustes

--- demo/app.py
+++ demo/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Ola DevOps {}'.format(os.environ.get("HOSTNAME"))
+    return 'Oi DevOpsDays \o/ {}'.format(os.environ.get("HOSTNAME"))
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8888)))
